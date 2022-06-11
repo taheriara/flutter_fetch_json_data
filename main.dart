ea305a97716fc1d8889ced98d0764f9d88a2c11f
taheriara <39693024+taheriara@users.noreply.github.com>
2022-06-11 10:51:31 +0430

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:web_view/api_call_json.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: FromServer(),
+    );
+  }
+}
